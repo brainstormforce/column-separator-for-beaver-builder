4c6db07c5b8fe9b9e225596439a7312f62d58a3f
Vijeta Raikar <110518282+vijetaR@users.noreply.github.com>
2024-07-11 11:36:51 +0530

    Updated changes

--- column-separator-for-beaver-builder.php
+++ column-separator-for-beaver-builder.php
@@ -4,8 +4,8 @@
  * Plugin URI: http://www.brainstormforce.com
  * Description: This is the plugin to create column separator in beaver builder.
  * Version: 1.0.2
- * Author: Brainstorm Force, Pratik Chaskar
- * Author URI: https://brainstormforce.com/
+ * Author: Pratik Chaskar
+ * Author URI: https://pratikchaskar.com/
  * License: GNU General Public License v3.0
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: bb-column-separator
